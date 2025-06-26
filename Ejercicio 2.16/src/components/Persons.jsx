bdbfa726b55d5a17f0317494fbe0eedcba9d4b30
Ángel <angelrmatoz@gmail.com>
2025-06-26 12:42:21 -0400

    feat: set up initial project structure with React, Vite, and essential components

---
+++ Ejercicio 2.16/src/components/Persons.jsx
@@ -0,0 +1,14 @@
+const Persons = ({ persons, handleDelete }) => {
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={person.name}>
+          {person.name} {person.number}
+          <button onClick={() => handleDelete(person.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Persons;
